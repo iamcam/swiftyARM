5ea11c6e5428c9cef346bf88f2a4bdc4ad069e27
Cameron Perry <mistercameron@gmail.com>
2016-06-09 00:25:01 -0700

    Fixed a bug and updated flashing rate on LED operated in the thread

--- readTemp/main.swift
+++ readTemp/main.swift
@@ -4,19 +4,29 @@ import Glibc
 let gpios = SwiftyGPIO.GPIOs(for: .RaspberryPi2)
 var led = gpios[.P6]!
 var tLED = gpios[.P21]!
-var blink = false
+
+//Clearer LED states
+let ON = 1
+let OFF = 0
+
+var blink = false {
+    didSet {
+        tLED.value = OFF
+    }
+}
 
 let probeNames = ["28-03159199a5ff"]
 let probeDirectory = "/sys/bus/w1/devices/"
 
 func loadIndicator() {
      led.direction = .OUT
-     led.value = 0
+     led.value = OFF
      tLED.direction = .OUT
-     tLED.value = 0
+     tLED.value = OFF
 }
 
 
+//reads from the probe, returns temperature in degrees C
 func readProbe(name : String) -> Double {
      let path = "\(probeDirectory)\(name)/w1_slave"
      var outval = ""
@@ -25,7 +35,6 @@ func readProbe(name : String) -> Double {
      let fp = fopen(path, "r")
 
      // try reading from /sys/bus/w1/devices/{prob name}/w1_slave
-  
      if fp != nil {
      	var buf = [CChar](repeating:CChar(0), count:BUFSIZE)
 	while fgets(&buf, Int32(BUFSIZE), fp) != nil {
@@ -34,10 +43,7 @@ func readProbe(name : String) -> Double {
 	}
      }
 
-
-
      let temp : Double
-
      if let tempS = outval.split(byString:"t=").last, t = Double(tempS.trim()) {
         temp = Double(t)/1000.0
 	} else {
@@ -48,10 +54,14 @@ func readProbe(name : String) -> Double {
 }
 
 func doReading() -> Double{
-     led.value = 1
-     let temp = readProbe(name: probeNames.first!) * 1.8 + 32.0
+     //Turn LED on to indicate we've begun reading the temperature
+     led.value = ON
+     let temp = readProbe(name: probeNames.first!)
+
+     //sleep a little bit so we don't miss it. Not required
      usleep(1000)
-     led.value = 0
+     
+     led.value = OFF
      return temp
 }
 
@@ -62,10 +72,10 @@ func startThread() {
           blink = true
           thread = NSThread() {
 	  	 while blink {
-		     tLED.value = 1
-		     usleep(250000)
-		     tLED.value = 0
-		     usleep(250000)
+		     tLED.value = ON
+		     usleep(50000)
+		     tLED.value = OFF
+		     usleep(50000)
 		 }
 		 exit(0)
 		 
@@ -74,17 +84,16 @@ func startThread() {
 
      if let thread = thread {
      	thread.start()
-	thread.stop()
      }
 }
 
 startThread()
-sleep(3)
+sleep(2)
 loadIndicator()
 let probeValue = doReading()
 
-print("\(probeValue)°F")
+print("\(probeValue) °C / \(probeValue * 1.8 + 32.0)°F")
 
-sleep(3)
+sleep(2)
 blink = false
 
